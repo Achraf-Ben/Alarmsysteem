0e44d87f3609a773ab1c9d6b01d385deb4f896e3
Achraf-Ben <achraf.ben.a@outlook.com>
2017-10-24 23:55:44 +0200

    try except toegevoegd

--- sshconnect.py
+++ sshconnect.py
@@ -2,19 +2,20 @@ import paramiko
 
 
 def sendCommand(hostname,command):
-    'Sends a command over SSH to a remote host using SSH key authentication'
-    k = paramiko.RSAKey.from_private_key_file("/home/pi/privatekey.pem")
-    c = paramiko.SSHClient()
-    c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-    #print("connecting")
-    c.connect( hostname = hostname, username = "pi", password = "AlarmSystem" )
-    #print("connected")
-    execute = "Execute: {}".format(command)
-    stdin,stdout,stderr = c.exec_command(command)
-    print(stdout.read())
-    #print()
-    #print( "Errors")
-    print(stderr.read())
-    c.close()
-    return execute
+    """Sends a command over SSH to a remote host using SSH key authentication"""
+    try:
+        key = paramiko.RSAKey.from_private_key_file("/home/pi/privatekey.pem")
+        sshClient = paramiko.SSHClient()
+        sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        sshClient.connect(hostname=hostname, username="pi", pkey=key)  # Connect to host
+        stdin,stdout,stderr = sshClient.exec_command(command)  # Runs command
+        print(stdout.read())  # Print shell read out
+        print(stderr.read())  # Print shell errors
+        sshClient.close()
+        executed = "Executed: {}".format(command)
+        return executed
+    except paramiko.ssh_exception.NoValidConnectionsError:
+        print("Connection to the Alarm Server got disrupted, triggering Alarm...")
+        return False
+    except:
+        print("Another error occured please contact support.")
