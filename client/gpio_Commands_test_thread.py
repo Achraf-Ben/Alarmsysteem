577e8958514e51c47c9bd486f84d190911af738c
vera98x <31824625+vera98x@users.noreply.github.com>
2017-10-26 16:55:10 +0200

    Add files via upload

--- client/gpio_Commands_test_thread.py
+++ client/gpio_Commands_test_thread.py
@@ -3,6 +3,7 @@ import time
 from sshConnect import sendCommand
 import threading
 
+# geeft de gpio-pins nummer een naam
 rood = 3
 geel = 5
 groen = 7
@@ -12,6 +13,7 @@ button3 = 31
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
+# initialiseren van de hardware
 GPIO.setup(rood, GPIO.OUT)
 GPIO.setup(geel, GPIO.OUT)
 GPIO.setup(groen, GPIO.OUT)
@@ -19,11 +21,13 @@ GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(button3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+#bij het opstarten gaan eerst alle lampjes aan
 print("Booting up...")
 GPIO.output(rood, GPIO.HIGH)
 GPIO.output(geel, GPIO.HIGH)
 GPIO.output(groen, GPIO.HIGH)
 time.sleep(1)
+#alarm staat aan, dit is te zien door de groene led
 print("System running")
 GPIO.output(rood, GPIO.LOW)
 GPIO.output(geel, GPIO.LOW)
@@ -34,7 +38,12 @@ def updateTimeDatabase():
     print('updateTimeDatabase call!')
     while True:
         print('updateTimeDatabase, loop')
-        sendCommand("server.pi", "cd Alarmsysteem && python3 /home/pi/Alarmsysteem/updateTime.py")
+        try:
+            sendCommand("server.pi", "cd Alarmsysteem && python3 /home/pi/Alarmsysteem/updateTime.py")
+        except:
+            GPIO.output(rood, GPIO.HIGH)
+            GPIO.output(geel, GPIO.LOW)
+            GPIO.output(groen, GPIO.LOW)
         time.sleep(20)
 
 
@@ -42,16 +51,11 @@ def updateTimeDatabase():
 def alarmOn():
     if sendCommand("server.pi", "cd Alarmsysteem && python3 /home/pi/Alarmsysteem/alarmOn.py") == False:
         print("Cant connect to server alarm triggerd")
-        GPIO.output(rood, GPIO.HIGH)
-        GPIO.output(geel, GPIO.LOW)
-        GPIO.output(groen, GPIO.LOW)
     else:
         print("Alarm Triggerd")
-        sendCommand("server.pi", "cd Alarmsysteem && python3 /home/pi/Alarmsysteem/alarmOn.py")
-        GPIO.output(rood, GPIO.HIGH)
-        GPIO.output(geel, GPIO.LOW)
-        GPIO.output(groen, GPIO.LOW)
-
+    GPIO.output(rood, GPIO.HIGH)
+    GPIO.output(geel, GPIO.LOW)
+    GPIO.output(groen, GPIO.LOW)
 
 def alarmOff(escape):
     GPIO.output(rood, GPIO.LOW)
@@ -65,10 +69,8 @@ def alarmOff(escape):
             GPIO.output(groen, GPIO.LOW)
         else:
             print("Alarm Disarmed")
-            GPIO.output(rood, GPIO.LOW)
-            GPIO.output(geel, GPIO.LOW)
-            GPIO.output(groen, GPIO.HIGH)
 
+# Maakt nieuwe thread aan om de tijd weg te schrijven in de database
 threadUpdateTime = threading.Thread(target=updateTimeDatabase)
 threadUpdateTime.start()
 
@@ -78,8 +80,7 @@ while True:
     input_state2 = GPIO.input(button2)
     input_state3 = GPIO.input(button3)
 
-    # TODO: Update presence time
-    # sendCommand("justanothergeek1", "echo Presence > Presence.txt")
+    #kijkt of button 1/button 2 wordt ingedrukt
     if input_state1 == True or input_state2 == True:
         print('Button 1 or 2 is Pressed')  # Triggers Alarm
         GPIO.output(rood, GPIO.LOW)
@@ -91,6 +92,7 @@ while True:
         timeout_start = time.time()
 
         alarmEscaped = False
+        # geeft voor 5 seconden de tijd om het alarm te disablen
         while time.time() < timeout_start + timeout:
             input_state3 = GPIO.input(button3)
             if input_state3 == True:
