e8460b057317535673ed35bf51e25092922f2021
vera98x <31824625+vera98x@users.noreply.github.com>
2017-10-26 14:02:48 +0200

    Add files via upload

---
+++ server/updateTime.py
@@ -0,0 +1,15 @@
+import sqlite3
+import datetime
+
+#TODO: try error
+conn = sqlite3.connect("alarm_database")
+c = conn.cursor()
+
+tijd = datetime.datetime.now().strftime("%H:%M:%S")
+print(tijd)
+
+c.execute(
+        'UPDATE alarmering SET time = ? WHERE alarm_nr = 1', (tijd,))
+
+conn.commit()
+conn.close()
